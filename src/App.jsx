4746094dc2360d34cec47348e173ed878aad08cc
grecob <bryson.greco@gmail.com>
2024-04-23 18:52:16 -0500

    basic setup and home button

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import SideNav from './components/SideNav'
+import Header from './components/Header'
+function App() {
+
+  return (
+      <><Header /><SideNav /></>
+  )
+}
+
+export default App
---
+++ src/components/SideNav.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+import { React, useState} from 'react'
+import { AiOutlineMenu, AiOutlineHome} from 'react-icons/ai'
+const SideNav = () => {
+    const [navActive, setNavActive] = useState(true)
+
+    const handleNav = () =>{
+        setNavActive(!navActive)
+    };
+
+    return (
+        <div>
+            <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+            {
+                navActive ? (
+                    <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
+                    <a href='main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <AiOutlineHome size={20}/>
+                        <span className='pl-4'>Home</span>
+                    </a>
+                    </div>
+                ) : (
+                    <div></div>
+                )
+            }
+        </div>
+    );
+};
+
+export default SideNav
