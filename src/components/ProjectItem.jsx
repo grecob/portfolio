8bf46990f9cde9e09f1bbc48031f30738f46d303
grecob <bryson.greco@gmail.com>
2024-04-25 20:50:06 -0500

    redesign

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,21 +1,31 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { FaGithubSquare } from 'react-icons/fa'
 
-const ProjectItem = ({ img, title, description, link}) => {
+const ProjectItem = ({ img, title, description, link, techStack }) => {
     return (
-        <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-white to-[#001b5e] hover:z-10'>
+        <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-[#001b5e] hover:z-10'>
             <img src={img} alt="/" className='rounded-xl group-hover:opacity-10' />
             <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
                 <h3 className='text-2xl font-bold text-white tracking-wider text-center'>
                     {title}
                 </h3>
-            <p className='pb-4 pt-2 text-white text-center'>{description}</p>
-            <a href={link} target='_blank'>
-                <p className='text-center p-3 rounded-lg bg-white text-gray-700 cursor-pointer text-lg'>More Info</p>
-            </a>
-            </div>
+                <p className='pb-4 pt-2 text-white text-center'>{description}</p>
+                <p>
 
+                    <div className='flex justify-center'>
+                        {techStack && techStack.map((TechIcon, index) => (
+                            <TechIcon key={index} className='mx-2 mb-2 text-white text-3xl' />
+                        ))}
+                    </div>
+
+                    <a href={link} target='_blank'>
+                        <p className='text-center p-3 rounded-lg bg-white text-gray-700 cursor-pointer text-lg'>View Demo</p>
+                    </a>
+
+                </p>
+            </div>
         </div>
     )
 }
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
-import { React, useState} from 'react'
-import { AiOutlineMenu, AiOutlineHome, AiOutlineMail, AiOutlineProject} from 'react-icons/ai'
-import { FiFile } from 'react-icons/fi';
+import { React, useState } from 'react'
+import { AiOutlineMenu, AiOutlineHome, AiOutlineMail, AiOutlineProject } from 'react-icons/ai'
 import { MdWorkOutline } from 'react-icons/md';
+import './SideNav.css'
+import { LiaLinkedin } from 'react-icons/lia';
+import { GrGithub } from 'react-icons/gr';
 const SideNav = () => {
     const [navActive, setNavActive] = useState(false)
 
@@ -10,69 +12,73 @@ const SideNav = () => {
         setNavActive(!navActive)
     };
 
-    const openResumeAndHandleNav = () => {
-        window.open('/public/grecob_resume.pdf', '_blank');
-        handleNav;
-        console.log("AAA");
-    };
-
     return (
         <div>
-            <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden block'/>
+            <AiOutlineMenu onClick={handleNav} className='fixed top-4 right-4 z-[99] md:hidden block' />
             {
                 navActive ? (
-                    <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
-                    <a onClick={handleNav} href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 duira'>
-                        <AiOutlineHome size={20}/>
-                        <span className='pl-4'>Home</span>
-                    </a>
-                    
-                    <a onClick={handleNav} href='#projects' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 duira'>
-                        <AiOutlineProject size={20}/>
-                        <span className='pl-4'>Projects</span>
-                    </a>
+                    <div className='fixed w-full h-screen bg-black/25 flex flex-col justify-center items-center z-20'>
+                        <a onClick={handleNav} href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                            <AiOutlineHome size={20} />
+                            <span className='pl-4'>Home</span>
+                        </a>
 
-                    <a onClick={handleNav} href='#workExperience' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 duira'>
-                        <MdWorkOutline size={20}/>
-                        <span className='pl-4'>Work Experience</span>
-                    </a>
-                    <a onClick={openResumeAndHandleNav} href='#main'className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 duira'>
-                        <FiFile size={20}/>
-                        <span className='pl-4'>Resume</span>
-                    </a>
+                        <a onClick={handleNav} href='#projects' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                            <AiOutlineProject size={20} />
+                            <span className='pl-4'>Projects</span>
+                        </a>
 
-                    <a onClick={handleNav} href='#contact' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 duira'>
-                        <AiOutlineMail size={20}/>
-                        <span className='pl-4'>Contact</span>
-                    </a>
-                    
-                    
+                        <a onClick={handleNav} href='#workExperience' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                            <MdWorkOutline size={20} />
+                            <span className='pl-4'>Work Experience</span>
+                        </a>
+
+                        <a onClick={handleNav} href='#contact' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                            <AiOutlineMail size={20} />
+                            <span className='pl-4'>Contact</span>
+                        </a>
+                        <a onClick={handleNav} href='https://www.linkedin.com/in/bryson-greco-15807a21b' target="_blank" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                            <LiaLinkedin size={20} />
+                            <span className='pl-4'>LinkedIn</span>
+                        </a>
+                        <a onClick={handleNav} href='https://www.github.com' target="_blank" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                            <GrGithub size={20} />
+                            <span className='pl-4'>GitHub</span>
+                        </a>
+
+
                     </div>
 
                 ) : ('')
             }
-
+            {/* icons */}
             <div className='md:block hidden fixed top-[25%] z-10'>
                 <div className='flex flex-col'>
-                    <a href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-300'>
+                    <a href='#main' className=' nav-button rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-300'>
                         <AiOutlineHome size={20} />
                     </a>
-                    <a href='#projects' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-300'>
+                    <a href='#projects' className='nav-button rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-300'>
                         <AiOutlineProject size={20} />
                     </a>
-                    <a href='#workExperience' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-300'>
+                    <a href='#workExperience' className='nav-button rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-300'>
                         <MdWorkOutline size={20} />
                     </a>
-                    <a href='#main' onClick={openResumeAndHandleNav} className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-300'>
-                        <FiFile size={20} />
-                    </a>
-                    <a href='#contact' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-300'>
+                    <a href='#contact' className='nav-button rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-300'>
                         <AiOutlineMail size={20} />
+                    </a>
+                    <a href='https://www.linkedin.com/in/bryson-greco-15807a21b' target="_blank" className='nav-button rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-300'>
+                        <LiaLinkedin size={20} />
+                    </a>
+                    <a href='https://www.github.com/grecob' target="_blank" className='nav-button rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-300'>
+                        <GrGithub size={20} />
                     </a>
                 </div>
             </div>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
 
         </div>
+
+
     );
 };
 
